9637376e743848c79deb3cd913e67fa99b259dcb
ashish200291 <69077534+ashish200291@users.noreply.github.com>
2022-10-09 11:43:26 +0530

    Update and rename All Paths From Source to Target - DFS.py to Find if Path Exists in Graph - BFS.py

---
+++ Find if Path Exists in Graph - BFS.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        
+        ad_list = [[] for _ in range(n)]
+        
+        for a,b in edges:
+            ad_list[a].append(b)
+            ad_list[b].append(a)
+            
+        q = deque([source])
+        visited = set()
+        
+        while q:
+            node = q.popleft()
+            if node == destination:
+                return True
+            
+            if node in visited:
+                continue
+            visited.add(node)
+            
+            for neighbor in ad_list[node]:
+                q.append(neighbor)
+        return False
