1386d3a12401162e9f7c2c5617afe396d60c82fe
ashish200291 <69077534+ashish200291@users.noreply.github.com>
2022-10-09 11:39:25 +0530

    Create All Paths From Source to Target - DFS.py

---
+++ All Paths From Source to Target - DFS.py
@@ -0,0 +1,19 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        def dfs(node):
+            path.append(node)
+            if node == len(graph) - 1:
+                paths.append(path.copy())
+                return
+
+            next_nodes = graph[node]
+            for next_node in next_nodes:
+                dfs(next_node)
+                path.pop()
+
+        paths = []
+        path = []
+        if not graph or len(graph) == 0:
+            return paths
+        dfs(0)
+        return paths
