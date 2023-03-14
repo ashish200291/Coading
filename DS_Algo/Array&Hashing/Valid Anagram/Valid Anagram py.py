db1859c4288d4ff4a53f0b170ba22744cc7f21e9
ashish200291 <69077534+ashish200291@users.noreply.github.com>
2023-03-15 00:58:20 +0530

    Update Valid Anagram py.py

--- DS_Algo/Array&Hashing/Valid Anagram/Valid Anagram py.py
+++ DS_Algo/Array&Hashing/Valid Anagram/Valid Anagram py.py
@@ -1,2 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        s_map ,t_map = [0]*26, [0]*26
+        for i in s:
+            s_map[ord(i)- 97] += 1
+
+        for i in t:
+            t_map[ord(i)-97] += 1
+        
+        for i in range(len(s_map)):
+            if s_map[i]!= t_map[i]:
+                return False
+
+        return True
