099f3d9b46bae6e0bfa2bc1ac60e0640b94c578b
ashish200291 <69077534+ashish200291@users.noreply.github.com>
2023-03-21 00:17:29 +0530

    Update Contains Duplicate py.py

--- DS_Algo/Array&Hashing/Contains Duplicate/Contains Duplicate py.py
+++ DS_Algo/Array&Hashing/Contains Duplicate/Contains Duplicate py.py
@@ -6,3 +6,14 @@ class Solution:
                 return True
             s.add(i)
         return False
+
+TC = O(N)
+SC = O(N)
+##############################################################
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return True if len(set(nums)) < len(nums) else False
+    
+TC = O(N)
+SC = O(N)
