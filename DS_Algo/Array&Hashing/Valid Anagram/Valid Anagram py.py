c0f3f97483ccdac09634427ed62793a2bd43a1c2
ashish200291 <69077534+ashish200291@users.noreply.github.com>
2023-03-20 23:49:20 +0530

    Update Valid Anagram py.py

--- DS_Algo/Array&Hashing/Valid Anagram/Valid Anagram py.py
+++ DS_Algo/Array&Hashing/Valid Anagram/Valid Anagram py.py
@@ -35,3 +35,14 @@ class Solution:
  TC: O(N)
  SC: O(N)
 ############################################################
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return collections.Counter(s) == collections.Counter(t)
+
+ 
+TC: O(N)
+SC: O(N)
+############################################################
